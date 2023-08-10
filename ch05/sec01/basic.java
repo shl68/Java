947b514f50d81af7a22e948f17f4636ee9d1f756
shl68 <105649474+shl68@users.noreply.github.com>
2023-08-10 22:56:30 +0900

    Create basic.java

---
+++ ch05/sec01/basic.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello world!");
+        System.out.println("a world!");
+        int a = 0;
+        String ab = "abc";
+        String var = null;
+        
+        float length = 0;
+
+
+        System.out.println(ab.length());
+
+        for (; a < 5; a++) {
+            System.out.println("1");
+        }
+
+
+            if(a == 0)
+            {
+                System.out.println(" a = 0");
+            }
+            else
+            {
+            System.out.println(" a =! 0");
+            }
+
+            System.out.println(a);
+
+            for ( ; length < 10; length++)
+            {
+
+            }
+
+        }
+
+
+    }
+
+
